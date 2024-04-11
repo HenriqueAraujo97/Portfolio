9da531ba9e636e0c7bc0f2f8791cb7e20a333d5a
Henrique <araujo.henrique311@gmail.com>
2024-04-12 00:42:19 +0100

    Created Header

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header = () => {
+  return (
+    <div className="header-wraper"> 
+    <div className="main-info"> 
+        <h1> web development and website promotion</h1>
+    </div>
+    </div>
+  )
+}
+
+export default Header
